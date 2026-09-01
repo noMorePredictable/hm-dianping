32fe53d9f6fbf60a2aae7f55907787b5b86bab14
AAAwjy <2191198424@qq.com>
2026-09-01 22:15:27 +0800

    Initial commit

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -27,29 +28,92 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Resource
     private StringRedisTemplate stringRedisTemplate;
     public Result queryById(Long id){
-
-    //1.从redis查找
+        //缓存穿透
+        //Shop shop = queryWithPassThrough(id);
+        //互斥锁解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        //返回
+        return Result.ok(shop);
+    }
+    public Shop queryWithMutex(Long id){
+        String key = "cache:shop:" + id;
+        //1.从redis查找
         String shopJson = stringRedisTemplate.opsForValue().get("cache:shop:" + id);
         //2.判断存在与否
         if (StrUtil.isNotBlank(shopJson)) {
             //3.存在返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
+        if (shopJson!=null) {
+            return null;
+        }
+        //4.实现缓存重建
+        // 4.1获取互斥锁
+        String lockkey = "lock:shop:" + id;
+        Shop shop = null;
+        try {
+            boolean islock = trylock(lockkey);
+            //4.2判断是否获取成功
+            if (islock) {
+                //4.2.1失败，休眠，重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
 
-    //4.不存在，查数据库
+
+            // 不存在，查数据库
+            shop = getById(id);
+            //5.不存在，返回错误
+            if (shop == null) {
+                //将空值写入redis
+                stringRedisTemplate.opsForValue().set("cache:shop:" + id,"",2L, TimeUnit.MINUTES);
+                return null;
+            }
+            //6.存在，写入redis
+            stringRedisTemplate.opsForValue().set("cache:shop:" + id, JSONUtil.toJsonStr(shop),30L, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }finally {
+            //释放互斥锁
+            unlock(lockkey);
+        }
+        //7.返回
+        return shop;
+    }
+    public Shop queryWithPassThrough(Long id){
+        String key = "cache:shop:" + id;
+        //1.从redis查找
+        String shopJson = stringRedisTemplate.opsForValue().get("cache:shop:" + id);
+        //2.判断存在与否
+        if (StrUtil.isNotBlank(shopJson)) {
+            //3.存在返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        if (shopJson!=null) {
+            return null;
+        }
+        //4.不存在，查数据库
         Shop shop = getById(id);
         //5.不存在，返回错误
         if (shop == null) {
-            return Result.fail("店铺不存在");
+            //将空值写入redis
+            stringRedisTemplate.opsForValue().set("cache:shop:" + id,"",2L, TimeUnit.MINUTES);
+            return null;
         }
-    //6.存在，写入redis
+        //6.存在，写入redis
         stringRedisTemplate.opsForValue().set("cache:shop:" + id, JSONUtil.toJsonStr(shop),30L, TimeUnit.MINUTES);
-    //7.返回
-    return Result.ok(shop);
-
-}
-
+        //7.返回
+        return shop;
+    }
+    private boolean trylock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
+    }
     @Override
     @Transactional
     public Result update(Shop shop) {
---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,15 @@
+package com.hmdp.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CacheClient {
+    private final StringRedisTemplate stringRedisTemplate;
+    public CacheClient(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+}
