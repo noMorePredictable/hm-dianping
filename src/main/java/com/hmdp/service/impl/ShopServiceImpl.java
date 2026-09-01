fe7b7c95a6a0c455afb859baf9efeea89dbd39c6
AAAwjy <2191198424@qq.com>
2026-09-01 16:45:36 +0800

    Initial commit

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
@@ -48,4 +49,16 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     return Result.ok(shop);
 
 }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if(id==null){
+            return Result.fail("店铺id不能为空");
+        }
+        updateById(shop);
+        stringRedisTemplate.delete( "cache:shop:" + id );
+        return Result.ok();
+    }
 }
